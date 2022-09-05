4916107548479f8e363283e57189bf322327d169
justine <106638324+justinekape@users.noreply.github.com>
2022-09-05 13:23:53 -0400

    integrated mongoose
    removed old code, organized CRUD,

--- index.js
+++ index.js
@@ -5,338 +5,204 @@ const express = require('express');
     bodyParser = require('body-parser');
      uuid = require('uuid');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+// integrating mongoose with REST API
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+
+// connects mongoose to db so it can perform CRUD ops on contained docs from within REST API
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-let users = [
-    {
-        id: 1,
-        name: 'hilo',
-        password: '1234',
-        email: 'kaulhilo@yahoo.com',
-        birthday: "1987-01-01",
-        favoriteMovies: []
-  },
-  {
-        id: 2,
-        name: 'shae',
-        password: '5678',
-        email: 'kaulshae@ymail.com',
-        birthday: "1989-01-01",
-        favoriteMovies: []
-  },
-  {
-        id: 3,
-        name: 'anden',
-        password: '0987',
-        email: 'emoryanden@gmail.com',
-        birthday: "1990-01-01",
-        favoriteMovies: []
-  },
-  {
-        id: 4,
-        name: 'Lan',
-        password: '4321',
-        email: 'kaullan@outlook.com',
-        birthday: "1980-01-01",
-        favoriteMovies: []
-  }
-]
+// *** CREATE ***  
 
-
-let topMovies = [
-    {
-        title: 'Silence of the Lambs',
-        description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.',
-        genre: {
-          name: 'Thriller',
-          description: 'Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience.'
-        },
-        director: {
-          name: 'Jonathan Demme',
-          bio: 'Robert Jonathan Demme was an American director, producer, and screenwriter.',
-          birth: '1944',
-          death: '2017'
-        },
-        imageUrl: 'silenceofthelambs.png',
-        featured: true
-      },
-      {
-        title: 'Mean Girls',
-        description: 'Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.',
-        genre: {
-          name: 'Comedy',
-          description: 'Comedy is a genre of film in which the main emphasis is humor.'
-        },
-        director: {
-          name: 'Mark Waters',
-          bio: 'Mark Stephen Waters is an American Filmmaker known for 500 Days of Summer, Vampire Academy and Ghosts of Girlfriends Past.',
-          birth: '1964',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0377092/mediaviewer/rm3978398720/?ref_=ext_shr_lnk',
-        Featured: true
-      },
-      {
-        title: 'Prey',
-        description: 'The origin story of the Predator in the world of the Comanche Nation 300 years ago. Naru, a skilled warrior, fights to protect her tribe against one of the first highly-evolved Predators to land on Earth.',
-        genre: {
-          name: 'Horror',
-          description: 'Horror is a film genre that seeks to elicit fear or disgust in its audience for entertainment purposes.'
-        },
-        director: {
-          name: 'Dan Trachtenberg',
-          bio: 'Dan Trachtenberg is an American filmmaker and podcast host. He directed the 2016 horror-thriller film 10 Cloverfield Lane which earned him a Directors Guild of America Award nomination for Outstanding Directing - First-Time Feature Film.',
-          birth: '1981',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt11866324/mediaviewer/rm4094888705/?ref_=ext_shr_lnk',
-        featured: true
-      },
-      {
-        title: 'Incantation',
-        description: 'Six years ago, Li Ronan was cursed after breaking a religious taboo. Now, she must protect her daughter from the consequences of her actions.',
-        genre: {
-          name: 'Horror',
-          description: 'Horror is a film genre that seeks to elicit fear or disgust in its audience for entertainment purposes.'
-        },
-        director: {
-          name: 'Kevin Ko',
-          bio: 'Taiwanese filmmaker Kevin KO came to prominence for a series of short films he made in college, including horror THE PRINT that screened at festivals in Japan and South Korea.',
-          birth: '1983',
-          death: '-'
-        },
-        imageUrl: '',
-        featured: true
-      },
-      {
-        title: 'Words Bubble Up Like Soda Pop',
-        description: 'A meeting and romance starts between two people with communication issues - a boy who wears headphones and uses haiku poems, and a girl who wears a mask and only does online videos.',
-        genre: {
-          name: 'Animation',
-          description: "Animation is a genre in which the film's images are primarily created by computer or hand and the characters are voiced by actors."
-        },
-        director: {
-          name: 'Kyohei Ishiguro',
-          bio: 'Kyōhei Ishiguro is a Japanese anime director. He debuted in 2009, and after doing the storyboards for three series, he was given the full directorial role in the anime adaptation of Your Lie in April. After directing other television series, he debuted as a film director with Words Bubble Up Like Soda Pop.',
-          birth: '1980',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt12735338/mediaviewer/rm1137114625/?ref_=ext_shr_lnk',
-        featured: true
-      },
-      {
-        title: 'The Wind Rises',
-        description: 'The Wind Rises is a fictionalised biographical film of Jiro Horikoshi, designer of the Mitsubishi A5M fighter aircraft and its successor, the Mitsubishi A6M Zero, used by the Empire of Japan during World War II.',
-        genre: {
-          name: 'Animation',
-          description: "Animation is a genre in which the film's images are primarily created by computer or hand and the characters are voiced by actors."
-        },
-        director: {
-          name: 'Hayao Miyazaki',
-          bio: 'Hayao Miyazaki is a Japanese animator, director, producer, screenwriter, author, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.',
-          birth: '1941',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt2013293/mediaviewer/rm2695221760/?ref_=tt_ov_i',
-        featured: true
-      },
-      {
-        title: 'Kikis Delivery Service',
-        description: 'A young witch, on her mandatory year of independent life, finds fitting into a new community difficult while she supports herself by running an air courier service.',
-        genre: {
-          name: 'Animation',
-          description: "Animation is a genre in which the film's images are primarily created by computer or hand and the characters are voiced by actors."
-        },
-        director: {
-          name: 'Hayao Miyazaki',
-          bio: 'Hayao Miyazaki is a Japanese animator, director, producer, screenwriter, author, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.',
-          birth: '1941',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0097814/mediaviewer/rm315211009/?ref_=ext_shr_lnk',
-        featured: true
-      },
-      {
-        title: 'My Neighbor Totoro',
-        description: 'When two girls move to the country to be near their ailing mother, they have adventures with the wondrous forest spirits who live nearby.',
-        genre: {
-          name: 'Animation',
-          description: "Animation is a genre in which the film's images are primarily created by computer or hand and the characters are voiced by actors."
-        },
-        director: {
-          name: 'Hayao Miyazaki',
-          bio: 'Hayao Miyazaki is a Japanese animator, director, producer, screenwriter, author, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.',
-          birth: '1941',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0096283/mediaviewer/rm4095130625/?ref_=ext_shr_lnk',
-        featured: true
-      },
-      {
-        title: "Howl's Moving Castle",
-        description: 'When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.',
-        genre: {
-          name: 'Animation',
-          description: "Animation is a genre in which the film's images are primarily created by computer or hand and the characters are voiced by actors."
-        },
-        director: {
-          name: 'Hayao Miyazaki',
-          bio: 'Hayao Miyazaki is a Japanese animator, director, producer, screenwriter, author, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.',
-          birth: '1941',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0347149/mediaviewer/rm2848505089/?ref_=ext_shr_lnk',
-        featured: true
-      },
-      {
-        title: 'Spirited Away',
-        description: "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
-        genre: {
-          name: 'Animation',
-          description: "Animation is a genre in which the film's images are primarily created by computer or hand and the characters are voiced by actors."
-        },
-        director: {
-          name: 'Hayao Miyazaki',
-          bio: 'Hayao Miyazaki is a Japanese animator, director, producer, screenwriter, author, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.',
-          birth: '1941',
-          death: '-'
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0245429/mediaviewer/rm4207852801/?ref_=ext_shr_lnk',
-        featured: true
-      }
-    
-];
-
-// CREATE
+// add a user
 app.post('/users', (req, res) => {
-    const newUser = req.body;
-
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('users need names')
-    }
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+          });
+      }
+    })
 })
 
-// UPDATE
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
+// add a movie to a user's favorites list
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $push: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true },
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     } else {
-        res.status(400).send('no such user');
+      res.json(updatedUser);
     }
-})
-
-
-// CREATE
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to ${user.name}'s favorites`);
-    } else {
-        res.status(400).send('no such user');
-    }
-})
-
-// DELETE
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from ${user.name}'s favorites`);
-    } else {
-        res.status(400).send('no such user');
-    }
-})
-
-// DELETE
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        users = users.filter(user => user.id != id);
-        res.status(200).send(`${user.name} has been deleted`);
-    } else {
-        res.status(400).send('no such user');
-    }
-})
-
-// READ
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
+  });
 });
 
+// *** READ *** 
+
+// default response
 app.get('/', (req, res) => {
-    res.send('What should I watch?');
+  res.send('Welcome to myFlix! What do you want to watch?');
 });
 
-
-
-// READ
+// all movies
 app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
-})
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-// READ
-app.get('/movies/:title', (req, res) => {
-    const { title } = req.params;
-    const movie = topMovies.find( movie => movie.title === title );
+// all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(400).send('no such movie')
+// find a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// find a movie by title
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// find genre info
+app.get('/genre/:Name', (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.Name })
+    .then((movies) => {
+      res.json(movies.Genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// find director info by name
+app.get('/director/:Name', (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.Name })
+    .then((movies) => {
+      res.json(movies.Director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// *** UPDATE ***  
+
+// update user's info by username
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set: 
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
     }
-})
-
-// READ
-app.get('/movies/genre/:genreName', (req, res) => {
-    const { genreName } = req.params;
-    const genre = topMovies.find( movie => movie.genre.name === genreName ).genre;
-
-    if (genre) {
-        res.status(200).json(genre);
+  },
+  { new: true },
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     } else {
-        res.status(400).send('no such movie')
+      res.json(updatedUser);
     }
-})
+});
+});
 
-// READ
-app.get('/movies/directors/:directorName', (req, res) => {
-    const { directorName } = req.params;
-    const director = topMovies.find( movie => movie.director.name === directorName ).director;
+// *** DELETE *** 
 
-    if (director) {
-        res.status(200).json(director);
+// delete a user
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// remove a movie from favorites
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true },
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     } else {
-        res.status(400).send('no such director')
+      res.json(updatedUser);
     }
-})
-
-
-
+  });
+});
 
 // ERROR HANDLING CODE
 app.use((err, req, res, next) => {
